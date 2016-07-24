7a98eea962d86769cfce49df5e1c9a84b4a8d5ea
yurik1776 <yurik1776@gmail.com>
2016-07-24 14:26:15 +0300

    added autocomplete and set map to autocomplete location

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,19 @@
  */
 (function(){
     window.initMap = function (){
-        console.log('initMap()')
         var map = new google.maps.Map(document.getElementById('map'),{
             center:{lat:26,lng:63},
-            scrollwheel:false,
+            scrollwheel:true,
             zoom:8
         })
-    }
+
+        var input = document.getElementById('search');
+        var autocomplete = new google.maps.places.Autocomplete(input,{
+            types: ['(cities)']
+        });
+        autocomplete.addListener('place_changed',function(){
+            var new_center = autocomplete.getPlace().geometry.location;
+            map.setCenter(new_center);
+        });
+    };
 })();
