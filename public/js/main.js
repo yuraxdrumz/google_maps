2f0cc99ad898a31a171a81b8a213ed668c49a9fa
yurik1776 <yurik1776@gmail.com>
2016-07-30 02:59:12 +0300

    fixed home css properties, added a few css lines on main

--- public/js/main.js
+++ public/js/main.js
@@ -39,21 +39,21 @@ $(document).ready(function(){
                     var place = document.createElement('div');
                     var img = document.createElement('img');
                     var review = document.createElement('div');
-                    img.className += 'img_size'
+                    img.className += 'img_size';
                     var user_name = document.createElement('div');
                     var createDate = moment(reviews[i].date).format("dddd, MMMM Do YYYY, h:mm:ss a");
                     date.innerHTML = createDate;
                     place.innerHTML = 'place reviewed: ' + reviews[i].place;
                     img.style.background = 'url('+reviews[i].img_url+') 50% 50% no-repeat';
-                    img.style.backgroundSize = 'cover'
-                    review.innerHTML = reviews[i].review
+                    img.style.backgroundSize = 'cover';
+                    review.innerHTML = reviews[i].review;
                     user_name.innerHTML ='by: ' + reviews[i].user_fname + ' ' + reviews[i].user_lname;
-                    div.appendChild(date)
-                    div.appendChild(place)
-                    div.appendChild(img)
-                    div.appendChild(user_name)
+                    div.appendChild(date);
+                    div.appendChild(place);
+                    div.appendChild(img);
+                    div.appendChild(user_name);
                     div.appendChild(review);
-                    div.className += 'single_review'
+                    div.className += 'single_review';
                     document.querySelector('.reviews').appendChild(div);
                 }
 
