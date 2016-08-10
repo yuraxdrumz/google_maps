4866e36f060ffa54130741ee2502fa9b434e71b5
yurik1776 <yurik1776@gmail.com>
2016-08-11 02:06:40 +0300

    added testing, need to add auth to check redirect

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(express.static(__dirname + '/src/public'));
 app.use(express.static(__dirname + '/node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(session({
--- test/check-server.js
+++ test/check-server.js
@@ -8,52 +8,105 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Blobs', function() {
-        beforeEach(function(done) { //Before each test we empty the database
-        User.remove({}, function(err){
-           done();
-        });
-    })
 
-  it('should get /', function(done) {
-    chai.request(server)
-    .get('/')
-    .end(function(err, res){
-      res.should.have.status(200);
-      done();
-    });
-  });
-    it('should get register on /register', function(done){
-        chai.request(server)
-        .get('/register')
-        .end(function(err,res){
-            res.should.have.status(200);
+describe('Users', function() {
+    beforeEach(function(done) { //Before each test we empty the database
+        User.remove({}, function(err){
+            console.log('removed');
             done();
+        });
+    });
+
+    describe('Get test', function() {
+      it('should get /', function(done) {
+        chai.request(server)
+        .get('/')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+      });
+    });
+
+    describe('Register Test',function(){
+        it('should get register on /register', function(done){
+            chai.request(server)
+            .get('/register')
+            .end(function(err,res){
+                res.should.have.status(200);
+                done();
+
+            })
+        });
+    });
+
+    describe('Register Post',function(){
+       it('should show post register /register', function(done){
+            var user = new User({
+                _id: mongoose.Types.ObjectId(),
+                first_name: 'yura',
+                last_name: 'khomyakov',
+                email: 'yura@gmail.com'
+                });
+                user.setPassword('444444');
+                chai.request(server)
+                .post('/register')
+                .send(user)
+                .end(function(err, res){
+                    res.should.have.status(200);
+                    done();
+                })
+        })
+
+    });
+    describe('Login Post', function(){
+        it('show show post login /login', function(done){
+            var user = new User({
+                _id: mongoose.Types.ObjectId(),
+                first_name: 'yura',
+                last_name: 'khomyakov',
+                email: 'yura@gmail.com'
+                });
+                user.setPassword('444444');
+                chai.request(server)
+                .post('/login')
+                .send(user)
+                .end(function(err, res){
+                    res.should.have.status(200);
+                    done();
+                })
         })
     });
-    it('show post login /login', function(done){
-        var user = new User({
-            _id: mongoose.Types.ObjectId(),
-            first_name: 'yura',
-            last_name: 'khomyakov',
-            email: 'yura@gmail.com'
-            });
-            user.setPassword('444444');
+    describe('Logout get', function(){
+        it('should logout',function(done){
             chai.request(server)
-            .post('/login')
-            .send(user)
-            .set('Accept', 'application/vnd.burgers.api+json')
+            .get('/logout')
             .end(function(err, res){
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.user.should.have.property('_id');
-                res.body.user.should.have.property('first_name');
-                res.body.user.should.have.property('last_name');
-                res.body.user.should.have.property('email');
-                res.body.user.should.have.property('salt');
-                res.body.user.should.have.property('hash');
                 done();
             })
+        })
+    });
+    describe('Main reject', function(){
+        it('should reject main',function(done){
+            chai.request(server)
+            .get('/main')
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            })
+        })
+    });
+    describe('Error 404', function(){
+        it('should return 404', function(done){
+            chai.request(server)
+            .get('/blabla')
+            .end(function(err,res){
+                res.should.have.status(404);
+                done();
+            })
+        })
     })
-
 });
+
+
